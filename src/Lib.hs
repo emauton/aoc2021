e1d051fe8e624d7a12c8a55f29fa178a206c7a98
Cian Synnott <cian@emauton.org>
2021-12-01 16:01:30 +0000

    Day 1 part 1: countIncreases implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      countIncreases
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "hello aoc2021"
+
+accumulate :: (Int, Int) -> Int -> (Int, Int)
+accumulate (prev, acc) x = if x > prev
+                             then (x, acc + 1)
+                             else (x, acc)
+
+countIncreases :: [Int] -> Int
+countIncreases numbers = snd (foldl accumulate ((head numbers), 0) (tail numbers))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "day 1 countIncreases" $ do
+    it "works on AoC example" $ do
+      countIncreases [199, 200, 208, 210, 200, 207, 240, 269, 260, 263] `shouldBe` (7 :: Int)
